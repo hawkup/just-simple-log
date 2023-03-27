a32338ee606cdd2ff9ab1555ce2abd08b2155336
hawkup <spiritsquare@gmail.com>
2023-03-27 13:16:40 +0700

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,18 @@ type Options = {
   tabWidth: number
 }
 
-export default {
-  log(args: unknown, options: Options = { tabWidth: 2 }) {
-    console.log(JSON.stringify(args, null, options.tabWidth))
-  },
-  error(args: unknown, options: Options = { tabWidth: 2 }) {
-    console.error(JSON.stringify(args, null, options.tabWidth))
-  },
-  warn(args: unknown, options: Options = { tabWidth: 2 }) {
-    console.warn(JSON.stringify(args, null, options.tabWidth))
-  },
-  info(args: unknown, options: Options = { tabWidth: 2 }) {
-    console.info(JSON.stringify(args, null, options.tabWidth))
-  }
+export const log = (args: unknown, options: Options = { tabWidth: 2 }) => {
+  console.log(JSON.stringify(args, null, options.tabWidth))
+}
+
+export const error = (args: unknown, options: Options = { tabWidth: 2 }) => {
+  console.error(JSON.stringify(args, null, options.tabWidth))
+}
+
+export const warn = (args: unknown, options: Options = { tabWidth: 2 }) => {
+  console.warn(JSON.stringify(args, null, options.tabWidth))
+}
+
+export const info = (args: unknown, options: Options = { tabWidth: 2 }) => {
+  console.info(JSON.stringify(args, null, options.tabWidth))
 }
