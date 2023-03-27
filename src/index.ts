b189b6fd8d239f4b6a989ed9ef2c00dfad7a06eb
hawkup <spiritsquare@gmail.com>
2023-03-27 12:44:30 +0700

    Add tabWidth options

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
+type Options = {
+  tabWidth: number
+}
+
 export default {
-  log(args: unknown) {
-    console.log(JSON.stringify(args, null, 2))
+  log(args: unknown, options: Options = { tabWidth: 2 }) {
+    console.log(JSON.stringify(args, null, options.tabWidth))
   },
-  error(args: unknown) {
-    console.error(JSON.stringify(args, null, 2))
+  error(args: unknown, options: Options = { tabWidth: 2 }) {
+    console.error(JSON.stringify(args, null, options.tabWidth))
   },
-  warn(args: unknown) {
-    console.warn(JSON.stringify(args, null, 2))
+  warn(args: unknown, options: Options = { tabWidth: 2 }) {
+    console.warn(JSON.stringify(args, null, options.tabWidth))
   },
-  info(args: unknown) {
-    console.info(JSON.stringify(args, null, 2))
+  info(args: unknown, options: Options = { tabWidth: 2 }) {
+    console.info(JSON.stringify(args, null, options.tabWidth))
   }
 }
